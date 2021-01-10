eb11fc2412dfe4e8bafb7a24f78908939194515e
D0Dam <skynickmj@naver.com>
2021-01-10 04:50:21 +0000

    add more commit 2021.01.09

--- src/stories/LifecycleStory.jsx
+++ src/stories/LifecycleStory.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { PageWithLoadData} from '../r05/lifecycle';
+import { PageWithLoadData, PageWithLoadDataAndLoading } from '../r05/lifecycle';
 
 storiesOf('Lifecycle', module)
   .addWithJSX('loadData 예제', () => (
     <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />
-  ));
+  ))
+	.addWithJSX('로딩 메시지 예제', () => (
+		<PageWithLoadDataAndLoading loadData={() => fetch('/').then(() => 'hellllo')} />
+	));
